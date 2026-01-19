5f6dbf2191844e1ae31fa23e6a42a5042d9e1ed7
Mahdee Al Wakil <121925483+drmaw@users.noreply.github.com>
2026-01-19 23:56:35 +0600

    Delete src directory

--- src/app/dashboard/messages/page.tsx
+++
@@ -1,6 +0,0 @@
-'use client';
-
-// This page has been deprecated and its functionality removed.
-export default function MessagesPage() {
-  return null;
-}
--- src/firebase/config.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  "projectId": "studio-2082838457-73814",
-  "appId": "1:763284989434:web:8e7b26b51e1d2fdfaef043",
-  "storageBucket": "studio-2082838457-73814.appspot.com",
-  "apiKey": "AIzaSyAr48XfHu_nBFZ4DosbBZ5nHZ3Kca6xMCE",
-  "authDomain": "studio-2082838457-73814.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "763284989434"
-};
